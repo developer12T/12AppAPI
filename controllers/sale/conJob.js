26d02677e8502171f49abf52effb225960b3b49f
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-06-16 13:09:51 +0700

    fix conjob 1 min

--- controllers/sale/conJob.js
+++ controllers/sale/conJob.js
@@ -132,14 +132,14 @@ async function erpApiCheckDisributionM3Job(channel = 'cash') {
 
 
 const startCronJobErpApiCheck = () => {
-  cron.schedule('*/50 * * * *', async () => {
+  cron.schedule('*/1 * * * *', async () => {
     console.log('Running cron job startCronJobErpApiCheck every 5 minutes')
     await erpApiCheckOrderJob()
   })
 }
 
 const startCronJobErpApiCheckDisribution = () => {
-  cron.schedule('*/50 * * * *', async () => {
+  cron.schedule('*/1 * * * *', async () => {
     console.log('Running cron job startCronJobErpApiCheckDisribution every 5 minutes')
     await erpApiCheckDisributionM3Job()
   })
