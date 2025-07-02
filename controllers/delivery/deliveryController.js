881040cb1d027b8c8eef819376ee0e947ac1f243
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-07-02 11:02:56 +0700

    Delivery add transaction and socket

--- controllers/delivery/deliveryController.js
+++ controllers/delivery/deliveryController.js
@@ -1,47 +1,54 @@
-const  deliveryModel  = require('../../models/cash/delivery')
+const deliveryModel = require('../../models/cash/delivery')
 const { getModelsByChannel } = require('../../middleware/channel')
+const { getSocket } = require('../../socket')
 
-
-exports.addDelivery = async (req,res) => {
+exports.addDelivery = async (req, res) => {
+    const session = await require('mongoose').startSession();
+    session.startTransaction();
     try {
-
-        const { stratMonth } = req.body
+        const { stratMonth } = req.body;
 
         const channel = req.headers['x-channel'];
-        const { Delivery } = getModelsByChannel(channel,res,deliveryModel); 
+        const { Delivery } = getModelsByChannel(channel, res, deliveryModel);
 
         const deliveryDateStart = new Date(`${stratMonth}-01`);
-        const deliveryExit = await Delivery.findOne({deliveryDateStart:deliveryDateStart})
+        const deliveryExit = await Delivery.findOne({ deliveryDateStart: deliveryDateStart }).session(session);
         if (deliveryExit) {
-
+            await session.abortTransaction();
+            session.endSession();
             return res.status(400).json({
-                Message:"Already have in database"
-            })
+                Message: "Already have in database"
+            });
         }
-        
+
         const deliveryDateEnd = new Date(deliveryDateStart);
         deliveryDateEnd.setMonth(deliveryDateEnd.getMonth() + 1);
         deliveryDateEnd.setDate(5);
-        const formatDate = date => date.toISOString().slice(0, 10);
 
-        preparationDays = 3
-        displayDays = 2
+        const preparationDays = 3; // ต้องมี let/const
+        const displayDays = 2;
 
-        await Delivery.create({
-        deliveryDateStart,
-        deliveryDateEnd,
-        preparationDays,
-        displayDays
+        await Delivery.create([{
+            deliveryDateStart,
+            deliveryDateEnd,
+            preparationDays,
+            displayDays
+        }], { session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        const io = getSocket()
+        io.emit('delivery/addDelivery', {});
+
+        res.status(200).json({
+            status: 200,
+            Message: "Add deliveryDate Success"
         });
 
-
-    res.status(200).json({
-        status:200,
-        Message:"Add deliveryDate Sucess"
-    })
-    
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ status: '500', message: error.message })
+        await session.abortTransaction().catch(() => { });
+        session.endSession();
+        res.status(500).json({ status: 500, message: error.message });
     }
-}
+};
