dd674aed7a02b694ff1a360c71957aeba604778c
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-10-16 08:45:57 +0700

    fix error store

--- controllers/store/storeController.js
+++ controllers/store/storeController.js
@@ -161,7 +161,7 @@ exports.getDetailStore = async (req, res) => {
 
     const store = {
       ...storeData,
-      storeType: typeStores.type || []
+      storeType: typeStores?.type || []
     }
 
     // const io = getSocket()
@@ -766,7 +766,7 @@ exports.checkSimilarStores = async (req, res) => {
 
   const existingStores = await Store.find(
     {
-      zone: store.zone,
+      // zone: store.zone,
       storeId: { $ne: storeId },
       $expr: { $lte: [{ $strLenCP: '$storeId' }, 12] } // ≤ 12 ตัวอักษร
     },
