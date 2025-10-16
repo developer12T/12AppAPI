07c669d81a4111098954e633af440d92e3c1cc33
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-10-16 11:02:38 +0700

    add restock

--- middleware/test.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const { sendEmail } = require('../middleware/order') // ปรับ path ให้ถูก
-
-sendEmail({
-  to: 'aukrit.chi@onetwotrading.co.th',
-  subject: 'แจ้งเตือนระบบ',
-  html: '<h1>ทดสอบส่งเมล</h1><p>ระบบแจ้งเตือนทำงานแล้ว</p>',
-})
