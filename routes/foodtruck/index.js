5f3f9894ca3be84c08fabd58f08dcb921c4eb888
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-10-17 14:27:48 +0700

    add addSendMoneyImageMuti

--- routes/foodtruck/index.js
+++ routes/foodtruck/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const noodleCartRoute = require('./noodleCartRoute')
 const noodleItemRoute = require('./noodleItemRoute')
 const noodleSaleRoute = require('./noodleSaleRoute')
+const noodleMoneyRoute = require('./noodleMoneyRoute')
 const router = express.Router()
 
 
 router.use('/noodleCart', noodleCartRoute)
 router.use('/noodleItem', noodleItemRoute)
 router.use('/noodlesale', noodleSaleRoute)
+router.use('/noodleMoney', noodleMoneyRoute)
 module.exports = router
