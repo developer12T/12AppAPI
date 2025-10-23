47f1118ebda864b50ce419ec5a87b2c31d2f9e49
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-10-23 10:01:47 +0700

    add npdArea

--- models/cash/npd.js
+++ models/cash/npd.js
@@ -9,10 +9,24 @@ const npdSchema = mongoose.Schema({
   updatedAt: { type: Date, default: Date.now }
 })
 
+const npdAreaSchema = mongoose.Schema({
+  period: { type: String },
+  // qty: { type: Number, require: true },
+  unit: { type: String },
+  // areaGet: { type: [String], default: [] },
+  perArea:[{
+    area: {type: String, require: true},
+    qty: { type: Number, require: true }
+  }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+})
+
 
 
 module.exports = conn => {
   return {
-    Npd: conn.model('npd', npdSchema,'npd')
+    Npd: conn.model('npd', npdSchema,'npd'),
+    NpdArea: conn.model('npdArea', npdAreaSchema,'npdAreas'),
   }
 }
