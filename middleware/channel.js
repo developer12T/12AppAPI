1f20bb3c1ec44b5278e78c8d3ebbd10759e8f46e
Nopparat Munsuwan <nopparat.mun@onetwotrading.co.th>
2025-10-24 11:34:53 +0700

    fix getModelsByChannel

--- middleware/channel.js
+++ middleware/channel.js
@@ -1,29 +1,30 @@
 // const storeModel = require('../../models/cash/store');
-const { dbCA, dbCR,dbPC,dbUSER } = require('../config/db');
+const { dbCA, dbCR, dbPC, dbUSER } = require('../config/db')
 
-function getModelsByChannel(channel,res,model) {
-    let conn;
-  
-    switch (channel) {
-      case 'credit':
-        conn = dbCR;
-        break;
-      case 'cash':
-        conn = dbCA;
-        break;
-      case 'pc':
-        conn = dbPC;
-      case 'user':
-        conn = dbUSER;
-        break;
-      default:
-          res.status(400).json({
-            status: 400,
-            message: "channel is required or invalid"
-          });
-    }
-  
-    return model(conn); 
+function getModelsByChannel (channel, res, model) {
+  let conn
+
+  switch (channel) {
+    case 'credit':
+      conn = dbCR
+      break
+    case 'cash':
+      conn = dbCA
+      break
+    case 'pc':
+      conn = dbPC
+      break
+    case 'user':
+      conn = dbUSER
+      break
+    default:
+      res.status(400).json({
+        status: 400,
+        message: 'channel is required or invalid'
+      })
   }
-  
-  module.exports = { getModelsByChannel };
+
+  return model(conn)
+}
+
+module.exports = { getModelsByChannel }
