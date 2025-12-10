7b0c532acf35ec538969a835e413b9b5faa39f9a
Aukrit <aukrit.chi@onetwotrad.co.th>
2025-12-10 15:54:30 +0700

    fix new checkout withdraw

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const dbCR = mongoose.createConnection(process.env.CR_DB_URI)
 const dbPC = mongoose.createConnection(process.env.PC_DB_URI)
 const dbUSER = mongoose.createConnection(process.env.USER_DB_URI)
 const dbCA_UAT = mongoose.createConnection(process.env.CA_DB_URI_UAT)
-
+const dbPC_UAT = mongoose.createConnection(process.env.PC_DB_URI_UAT)
 // const foodServiceDB = mongoose.createConnection(process.env.FS_DB_URI)
 
 dbUSER.on('connected', () => console.log('Connected to User DB'))
@@ -23,10 +23,14 @@ dbPC.on('error', err => console.error('PC DB Error:', err))
 dbCA_UAT.on('connected', () => console.log('Connected to Cash DB UAT'))
 dbCA_UAT.on('error', err => console.error('Cash DB UAT Error:', err))
 
+dbPC_UAT.on('connected', () => console.log('Connected to PC DB UAT'))
+dbPC_UAT.on('error', err => console.error('Cash DB UAT Error:', err))
+
 module.exports = {
   dbCA,
   dbCR,
   dbPC,
   dbUSER,
-  dbCA_UAT
+  dbCA_UAT,
+  dbPC_UAT
 }
